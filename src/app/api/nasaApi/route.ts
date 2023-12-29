3c9e8ec3045f14581dbf3b451cea56f2bd29325c
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2023-12-29 19:06:07 -0300

    front end changes

--- src/app/api/nasaApi/route.ts
+++ src/app/api/nasaApi/route.ts
@@ -1,24 +1,22 @@
 import { NasaApiResponse } from "@/types/nasaApiTypes";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(res: NextApiResponse, req: NextApiRequest) {
     try {
-        const apiKey = process.env.API_KEY;
+        const apiKey = process.env.API_KEY
         if (!apiKey) {
-            throw new Error("La Api Key no está bien configurada!");
+            throw new Error("La Api Key no esta bien configurada!")
         }
-        const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-        const response = await fetch(apiUrl);
-
+        const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+        const response = await fetch(apiUrl)
         if (!response.ok) {
-            throw new Error(`Error al obtener la imagen del día. Código de estado: ${response.status}`);
+            throw new Error(`Error al obtener la imagen del día. Código de estado: ${response.status}`)
         }
-
         const data: NasaApiResponse = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        console.error("Error al obtener la imagen del día:", error);
-        res.status(500).json({ error: "Error al obtener la imagen del día." });
+        return new Response(JSON.stringify(data), { status: 200 })
+    }
+    catch (error) {
+        console.error("Error al obtener la imagen del dia:", error)
+        return new Response(JSON.stringify({ error: "Error al obtener la imagen del día." }), { status: 500 });
     }
 }
-
