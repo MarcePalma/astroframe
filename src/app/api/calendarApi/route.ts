2d3aceb134b5ea12544003cd10bf7cbf880a4b7d
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2024-01-07 18:58:48 -0300

    api changes

--- src/app/api/calendarApi/route.ts
+++ src/app/api/calendarApi/route.ts
@@ -1,35 +1,35 @@
 import { NasaApiResponse } from "@/types/nasaApiTypes";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
-      const apiKey = process.env.API_KEY;
-      if (!apiKey) {
-          throw new Error("La Api Key no está bien configurada!");
-      }
-      
-      console.log('Consulta recibida:', req.url);
+    const apiKey = process.env.API_KEY;
+    if (!apiKey) {
+      throw new Error("La Api Key no está bien configurada!");
+    }
 
-      const url = new URL(req.url || '', 'http://localhost');
-      const date = url.searchParams.get("date");
+    console.log('Consulta recibida:', req.url);
 
-      if (!date) {
-          console.error('La fecha no está presente en la consulta.');
-          return res.status(400).json({ error: 'La fecha no está presente en la consulta.' });
-      }
+    const url = new URL(req.url || '', 'http://localhost');
+    const date = url.searchParams.get("date");
 
-      const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+    if (!date) {
+      console.error('La fecha no está presente en la consulta.');
+      return new Response(JSON.stringify({ error: "La fecha no esta presenten en la consulta" }), { status: 400 })
+    }
 
-      const apiResponse = await fetch(apiUrl);
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
 
-      if (apiResponse.ok) {
-          const data: NasaApiResponse = await apiResponse.json();
-          return new Response(JSON.stringify(data), { status: 200 });
-      } else {
-          throw new Error(`Error al obtener la imagen del día. Código de estado: ${apiResponse.status}`);
-      }
+    const apiResponse = await fetch(apiUrl);
+
+    if (apiResponse.ok) {
+      const data: NasaApiResponse = await apiResponse.json();
+      return new Response(JSON.stringify(data), { status: 200 });
+    } else {
+      throw new Error(`Error al obtener la imagen del día. Código de estado: ${apiResponse.status}`);
+    }
   } catch (error) {
-      console.error("Error en /app/api/calendarApi/route.ts:", error);
-      return new Response(JSON.stringify({ error: "Error interno en el servidor." }), { status: 500 });
+    console.error("Error en /app/api/calendarApi/route.ts:", error);
+    return new Response(JSON.stringify({ error: "Error interno en el servidor." }), { status: 500 });
   }
 }
