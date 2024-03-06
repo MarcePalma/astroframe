3a27a71a40a563b96ce114f6a3c1f479a719e6da
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2024-03-06 06:21:34 -0300

    fetch bugfix

--- src/components/apod/Apod.tsx
+++ src/components/apod/Apod.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use-client"
 import React, { useEffect, useState } from 'react';
 import ApodCard from './ApodCard';
 import { NasaApiResponse } from '@/types/nasaApiTypes';
@@ -7,6 +6,7 @@ import { NasaApiResponse } from '@/types/nasaApiTypes';
 const Apod: React.FC = () => {
     const [apodData, setApodData] = useState<NasaApiResponse | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [lastFetchDate, setLastFetchDate] = useState<string | null>(null);
 
     useEffect(() => {
         const getApodFromServer = async () => {
@@ -21,14 +21,19 @@ const Apod: React.FC = () => {
 
                 const data: NasaApiResponse = await response.json();
                 setApodData(data);
+                setLastFetchDate(new Date().toISOString().split('T')[0]);
             } catch (error) {
                 console.error('Error al obtener la imagen del día desde el servidor:', error);
                 setError('Error al obtener la imagen del día. Por favor, verifica la consola para más detalles.');
             }
         };
 
-        getApodFromServer();
-    }, [new Date().toISOString().split('T')[0]]);
+        // Verificar si es la primera vez que se monta el componente o si han pasado más de un día desde la última actualización
+        const currentDate = new Date().toISOString().split('T')[0];
+        if (!lastFetchDate || lastFetchDate !== currentDate) {
+            getApodFromServer();
+        }
+    }, [lastFetchDate]);
 
     return (
         <div id='Apod'>
